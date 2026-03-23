a82d9352a42849e54b170c388de7accad758a1ef
Pavithra Muniraju <pavithra.muniraju@ust.com>
2026-03-23 15:28:54 +0530

    api changed

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyCKJ1V1_XA_Pvt2XxKefwUtnqRwElBGVVM'
+const GOOGLE_API_KEY = 'PAIzaSyCKJ1V1_XA_Pvt2XxKefwUtnqRwElBGVVM_UNIRAJU'
 
 export function getMapPreview(lat, lng) {
     const imagePreview = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng},NY&zoom=14&size=400x400&maptype=roadmap&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=${GOOGLE_API_KEY}`
